1bb261d5a33286cae9d6eff4ff9b33124e2698bc
Dhiraj Kumar Prasad <117366089+Dhirajkrp@users.noreply.github.com>
2023-10-27 10:33:06 +0530

    using typography styles

---
+++ typography.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+TextStyle h1 = const TextStyle(fontSize: 32 , fontWeight: FontWeight.w600);
+TextStyle h2 = const TextStyle(fontSize: 28 , fontWeight: FontWeight.w600);
+TextStyle h3 = const TextStyle(fontSize: 24 , fontWeight: FontWeight.w600);
+TextStyle h4 = const TextStyle(fontSize: 22 , fontWeight: FontWeight.w600);
+TextStyle h5 = const TextStyle(fontSize: 20 , fontWeight: FontWeight.w600);
+TextStyle h6 = const TextStyle(fontSize: 18 , fontWeight: FontWeight.w600);
+
+
+TextStyle p = const TextStyle(fontSize: 18 , fontWeight: FontWeight.w500);
